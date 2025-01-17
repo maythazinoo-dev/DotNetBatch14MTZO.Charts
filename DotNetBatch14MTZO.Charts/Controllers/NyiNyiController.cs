1afe34ba5cf049ae64899aa387de60f43faf4aaf
nyinyiminthant-dev <nnyi37389@gmail.com>
2025-01-18 01:39:45 +0630

    Feat: Finished Apex BarChart

--- DotNetBatch14MTZO.Charts/Controllers/NyiNyiController.cs
+++ DotNetBatch14MTZO.Charts/Controllers/NyiNyiController.cs
@@ -1,20 +1,133 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Xml.Linq;
+
+using DotNetBatch14MTZO.Charts.Models.NyiNyi;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace DotNetBatch14MTZO.Charts.Controllers
 {
     public class NyiNyiController : Controller
     {
+        
+
         [ActionName("barchart")]
         public IActionResult BarChart()
         {
-            return View();
-        }
+            BarChartModel model = new BarChartModel()
+            {
+                Series = new BarChartSeriModel[]
+                {
+                    new BarChartSeriModel
+                    {
+                        Name = "Actual",
+                        Data = new BarChartData[]
+                        {
+                            new BarChartData
+                            {
+                                X = "2011",
+                                Y = 12,
+                                Goals = new Goal[]
+                                {
+                                    new Goal
+                                    {
+                                        Name = "Expected",
+                                        Value = 14,
+                                        StrokeWidth = 2,
+                                        StrokeDashArray = 2,
+                                        StrokeColor = "#775DD0"
+                                    }
+                                }
+                            },
+                            new BarChartData
+                            {
+                                X = "2012",
+                                Y = 44,
+                                Goals = new Goal[]
+                                {
+                                    new Goal
+                                    {
+                                        Name = "Expected",
+                                        Value = 54,
+                                        StrokeWidth = 5,
+                                        StrokeHeight = 10,
+                                        StrokeColor = "#775DD0"
+                                    }
+                                }
+                            },
+                            new BarChartData
+                            {
+                                X = "2013",
+                                Y = 54,
+                                Goals = new Goal[]
+                                {
+                                    new Goal
+                                    {
+                                        Name = "Expected",
+                                        Value = 52,
+                                        StrokeWidth = 10,
+                                        StrokeHeight = 0,
+                                        StrokeLineCap = "round",
+                                        StrokeColor = "#775DD0"
+                                    }
+                                }
+                            },
+                            new BarChartData
+                            {
+                                X = "2014",
+                                Y = 66,
+                                Goals = new Goal[]
+                                {
+                                    new Goal
+                                    {
+                                        Name = "Expected",
+                                        Value = 61,
+                                        StrokeWidth = 10,
+                                        StrokeHeight = 0,
+                                        StrokeLineCap = "round",
+                                        StrokeColor = "#775DD0"
+                                    }
+                                }
+                            },
+                            new BarChartData
+                            {
+                                X = "2015",
+                                Y = 81,
+                                Goals = new Goal[]
+                                {
+                                    new Goal
+                                    {
+                                        Name = "Expected",
+                                        Value = 66,
+                                        StrokeWidth = 10,
+                                        StrokeHeight = 0,
+                                        StrokeLineCap = "round",
+                                        StrokeColor = "#775DD0"
+                                    }
+                                }
+                            },
+                            new BarChartData
+                            {
+                                X = "2016",
+                                Y = 67,
+                                Goals = new Goal[]
+                                {
+                                    new Goal
+                                    {
+                                        Name = "Expected",
+                                        Value = 70,
+                                        StrokeWidth = 5,
+                                        StrokeHeight = 10,
+                                        StrokeColor = "#775DD0"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
 
-        [ActionName("piechart")]
-        public IActionResult PieChart()
-        {
-            Console.WriteLine("PieChart action hit");
-            return View();
+            
+            return View(model);
         }
     }
 }
---
+++ DotNetBatch14MTZO.Charts/Models/NyiNyi/BarChartModel.cs
@@ -0,0 +1,31 @@
+﻿namespace DotNetBatch14MTZO.Charts.Models.NyiNyi
+{
+    public class BarChartModel
+    {
+        public BarChartSeriModel[] Series { get; set; }
+    }
+
+    public class BarChartSeriModel
+    {
+        public string Name { get; set; }
+        public BarChartData[] Data { get; set; }
+    }
+
+    public class BarChartData
+    {
+        public string X { get; set; }  
+        public int Y { get; set; }     
+        public Goal[] Goals { get; set; } 
+    }
+
+    public class Goal
+    {
+        public string Name { get; set; }  
+        public int Value { get; set; }    
+        public int StrokeWidth { get; set; }  
+        public int StrokeHeight { get; set; } 
+        public string StrokeColor { get; set; }
+        public string StrokeLineCap { get; set; } 
+        public int StrokeDashArray { get; set; } 
+    }
+}
