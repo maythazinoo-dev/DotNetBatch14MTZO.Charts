227c78161e9ba885366c2ce9b53d3b827f6c3f01
May Thazin Oo <maythazinoo18817@gmail.com>
2025-01-17 19:38:43 +0630

    Add project files.

---
+++ DotNetBatch14MTZO.Charts/Controllers/ApexChartsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetBatch14MTZO.Charts.Controllers
+{
+    public class ApexChartsController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+    }
+}
